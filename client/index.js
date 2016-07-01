52777a89c8d498ebe50aae18eaddc36112358329
chenxiaodong <cxdongjack@163.com>
2016-07-01 21:52:51 +0800

    feat: 增加手动添加用户功能

--- client/index.js
+++ client/index.js
@@ -1,2 +1,5 @@
+import "./templates/addGuestForm/addGuestForm.html";
+import "./templates/addGuestForm/addGuestForm.js";
+// Styles
 // Styles
 import "./styles/main.less";
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import "./methods/accounts.js";
 import initRegistry from "./register.js";
 
 initRegistry();
